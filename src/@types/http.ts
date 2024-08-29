1aea7438ebf06d6dd1d31f71c0ae9f325707ace6
João Seixas <joaoseixas88@gmail.com>
2024-08-29 11:18:13 -0300

    feat: create an starter HttpContextContract and User

---
+++ src/@types/http.ts
@@ -0,0 +1,28 @@
+import { User } from "./user";
+
+export interface HttpResponse<T = any> {
+  statusCode: number;
+  data: T;
+}
+
+export interface HttpContextContract {
+  request: {
+    allParams<T = any>(): T;
+    headers(): Record<string, string>;
+    queryParam(): Record<string, string>;
+    body<T = any>(): T;
+    params(): Record<string, string>;
+    auth: {
+      user?: User;
+    };
+  };
+  response: {
+    ok<T = any>(data: T): HttpResponse;
+    badRequest(data: any): HttpResponse;
+    noContent(data: any): HttpResponse;
+    internalServerError(): HttpResponse;
+    notFound(data?: any): HttpResponse;
+    created(): HttpResponse;
+    sendFile(filePath: string, fileName?: string): void;
+  };
+}
