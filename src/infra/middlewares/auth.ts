44133d33dea792619679be5c4ab7c05416c2aa26
João Seixas <joaoseixas88@gmail.com>
2024-08-30 08:55:29 -0300

    chore: create an auth middleware

---
+++ src/infra/middlewares/auth.ts
@@ -0,0 +1,30 @@
+import { RouterMiddleware } from "@/infra/config/adapters/RouteAdapter";
+import { JwtAuththorizer } from "@/infra/config/JwtAuthorizer";
+import { NextFunction, Request, Response } from "express";
+import { container } from "tsyringe";
+
+export const authMiddlewahe: RouterMiddleware = (
+  req: Request,
+  res: Response,
+  next: () => NextFunction
+): void => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    res.status(401).json("Authorization required");
+    return;
+  }
+  const [bearer, token] = authorization?.split(" ");
+  if (!token || bearer !== "Bearer") {
+    res.status(400).json("Malformed token");
+    return;
+  }
+  const authorizer = container.resolve(JwtAuththorizer);
+  const user = authorizer.verify(token);
+  if (!user) {
+    res.status(403).json("Not authorized");
+    return;
+  } else {
+    req.user = user;
+    next();
+  }
+};
